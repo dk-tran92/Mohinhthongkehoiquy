9e101de8cf0cd12cbb92e3d465625272ca961df9
dk-tran92 <61671443+dk-tran92@users.noreply.github.com>
2021-04-12 13:03:54 +0700

    Update PCA.R

--- PCA.R
+++ PCA.R
@@ -34,6 +34,23 @@ plot(pc)
 ####################################
 
 
+
+mtcars.pca <- prcomp(mtcars[,c(1:7,10,11)], center = TRUE,scale. = TRUE)
+
+summary(mtcars.pca)
+
+str(mtcars.pca)
+
+ggbiplot(mtcars.pca)
+ggbiplot(mtcars.pca, labels=rownames(mtcars))
+
+mtcars.country <- c(rep("Japan", 3), rep("US",4), rep("Europe", 7),rep("US",3), "Europe", rep("Japan", 3), rep("US",4), rep("Europe", 3), "US", rep("Europe", 3))
+
+ggbiplot(mtcars.pca,ellipse=TRUE,  labels=rownames(mtcars), groups=mtcars.country)
+
+
+
+
 #Cach2##############################
 wim.pca <- prcomp(clData[,c(3:38)], center = TRUE,scale. = TRUE)
 
@@ -47,8 +64,8 @@ library(devtools)
 install_github("vqv/ggbiplot")
 
 
- library(ggbiplot)
+library(ggbiplot)
 
-ggbiplot(mtcars.pca)
+ggbiplot(wim.pca)
 
 ####################################
